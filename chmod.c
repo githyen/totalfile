6a6424ad330a41d2740af852eef18c46ecd44483
hyen <76938461+githyen@users.noreply.github.com>
2021-07-12 21:04:22 +0900

    Add files via upload

---
+++ chmod.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
+#include<sys/stat.h>
+ 
+int main(int argc,char **argv)
+{        
+    if(argc != 3)
+    {
+        fprintf(stderr,"usage: %s <file name> <access mode>\n",argv[0]);
+        fprintf(stderr,"access mode is octet number. must start %s\n",argv[0]);
+        return 1;
+    }
+    if(access(argv[2],F_OK)!=0)
+    {
+        printf("%s is not existed\n",argv[1]);
+        return 1;
+    }
+ 
+    mode_t mode = 0;
+	if(argv[1][0]=='0')
+	{
+		sscanf(argv[1], "%o",&mode);
+		if(chmod(argv[2],mode)!=0)
+		    printf("fail change mode\n");
+		else
+			printf("success change mode\n");
+	 }
+	else if(argv[1][0] < '8')
+	{
+		sscanf(argv[1], "%o",&mode);
+
+		if(chmod(argv[2],mode)< 0)
+		    printf("fail change mode\n");
+		else
+			printf("success change mode\n");
+	}
+	else
+	{
+		fprintf(stderr,"chmod: 잘못된 모드: `%s' \n",argv[1]);
+		fprintf(stderr,"Try 'chmod --help' for more information.\n");
+		exit(1);
+	}
+    return 0;    
+}
---
+++ touch.c
@@ -0,0 +1,41 @@
+#include<sys/types.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<time.h>
+#include<utime.h>
+#include<sys/time.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main(int argc, char **argv)
+{
+	
+	struct utimbuf ubuf;
+	if(argc == 0)
+	{
+		fprintf(stderr,"touch: 파일 명령어 누락\nTry 'touch --help' for more information.\n");
+		exit(1);
+	}
+	if(strncmp(argv[1],"--",2) == 0)
+	{
+		fprintf(stderr,"touch: 인식할 수 없는 옵션 '%s'\nTry 'touch --help' for more information.\n",argv[1]);
+		exit(1);
+	}
+	for(int i=0; i<argc; i++)
+	{
+		if(access(argv[i],F_OK) < 0)
+		{
+			umask(0);
+			creat(argv[i],0644);
+		}
+	}
+	
+	ubuf.actime = time((time_t *)0);
+	ubuf.modtime = time((time_t *)0);
+
+    utime(argv[1], NULL);
+  //  utime(argv[1], &ubuf);
+    exit(0);
+}
